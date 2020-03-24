ccbc9eb9b13926a21b1b86eb7820d2a219bff065
bruno.cardoso <brunocpessoal@gmail.com>
2020-03-24 20:37:13 -0300

    route added to list and register ONGs

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const crypto = require('crypto');
 const routes = express.Router();
+const connection = require('./database/connection');
 
 routes.get('/', (req, res) => {
   return res.json({
     message: 'teste'
   })
+});
+
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return res.json({ id });
+});
+
+routes.get('/ongs', async (req, res) => {
+  const ongs = await connection('ongs').select('*');
+
+  return res.json(ongs);
 });
 
 module.exports = routes;
