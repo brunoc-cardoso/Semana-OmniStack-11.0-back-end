7d141f665ef76aafe2737f2b6ee0c351c65296c8
bruno.cardoso <brunocpessoal@gmail.com>
2020-03-24 22:42:39 -0300

    add profile and session routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
@@ -10,5 +14,7 @@ routes.post('/ongs', OngController.store);
 routes.post('/incidents', IncidentController.store);
 routes.get('/incidents', IncidentController.index);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
