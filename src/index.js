e31e0c29894d24aaa9d65b18250f65d1db122b0b
bruno.cardoso <brunocpessoal@gmail.com>
2020-03-27 12:54:15 -0300

    add validations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const app = express();
 const PORT = 3333;
@@ -8,6 +9,7 @@ const PORT = 3333;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(PORT);
 console.log(`Application running on port ${PORT}`);
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const { Joi, celebrate, Segments } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -9,12 +10,32 @@ const SessionController = require('./controllers/SessionController');
 routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), OngController.store);
 
 routes.post('/incidents', IncidentController.store);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentController.index);
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}), IncidentController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorizathion: Joi.string().required(),
+  }).unknown()
+}), ProfileController.index);
 
 module.exports = routes;
